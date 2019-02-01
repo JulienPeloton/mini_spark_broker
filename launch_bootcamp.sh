8273199da9aeec13c8884999b338c1fafe09de9e
Julien <peloton@lal.in2p3.fr>
2019-02-01 22:35:28 +0100

    Update bootcamp launcher

--- launch_bootcamp.sh
+++ launch_bootcamp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build the docker image (~6 GB!)
+# Slow the first time - super quick after.
+docker build -t "msb" .
+
 # Kafka dependencies
 KFKSTREAM=org.apache.spark:spark-streaming-kafka-0-10-assembly_2.10:2.2.0
 KFKSQL=org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0
