8dcfa0d59dd1f3b5176eb86d28edf24024310038
Julien <peloton@lal.in2p3.fr>
2019-02-04 15:54:35 +0100

    Update the LSST alert system for msb

--- bin/sendAlertStream.py
+++ bin/sendAlertStream.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
-
-"""Generates batches of alerts coming from a CCD given template alert
-content.
+# Copyright 2018 Julien Peloton
+# Author: Maria Patterson, Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Generates batches of alerts coming from ZTF.
 """
-
-from __future__ import print_function
 import argparse
 import glob
 import time
 import asyncio
 from lsst.msb import alertProducer, avroUtils
-
 
 @asyncio.coroutine
 def delay(wait_sec, function, *args):
@@ -48,19 +57,20 @@ def schedule_delays(eventloop, function, argslist, interval=39):
         counter += 1
     eventloop.stop()
 
-
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('broker', type=str,
-                        help='Hostname or IP and port of Kafka broker.')
-    parser.add_argument('topic', type=str,
-                        help='Name of Kafka topic stream to push to.')
+    parser.add_argument(
+        'broker', type=str,
+        help='Hostname or IP and port of Kafka broker.')
+    parser.add_argument(
+        'topic', type=str,
+        help='Name of Kafka topic stream to push to.')
     args = parser.parse_args()
 
     # Configure producer connection to Kafka broker
     conf = {'bootstrap.servers': args.broker}
     streamProducer = alertProducer.AlertProducer(
-                        args.topic, schema_files=None, **conf)
+        args.topic, schema_files=None, **conf)
 
     # Scan for avro files
     root = "./data"
@@ -71,20 +81,25 @@ def main():
         print('visit:', f[15:20], '\ttime:', time.time())
         # Load alert contents
         with open(f, mode='rb') as file_data:
+            # Read the data
             data = avroUtils.readSchemaData(file_data)
-            # TODO replace Avro files with visits having better S/N cut
-            # for now, limit to first 10,000 alerts (current have ~70,000)
+
+            # Read the Schema
+            schema = data.schema
+
+            # Send the alerts
             alert_count = 0
             for record in data:
                 if alert_count < 10000:
-                    streamProducer.send(record, encode=False)
+                    streamProducer.send(
+                        record, alert_schema=schema, encode=True)
                     alert_count += 1
                 else:
                     break
         streamProducer.flush()
 
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(schedule_delays(loop, send_visit, files, 1))
+    asyncio.ensure_future(schedule_delays(loop, send_visit, files, interval=1))
     loop.run_forever()
     loop.close()
 
