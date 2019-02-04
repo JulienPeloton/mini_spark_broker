2e926f4e31f5442e33366bf632f3ce9c43963366
Julien <peloton@lal.in2p3.fr>
2019-02-04 10:47:50 +0100

    Add script to launch the alert system

---
+++ launch_alert_system.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###################################
+# LSST Alert System
+###################################
+
+# Send bursts of alerts at expected visit intervals to topic "my-stream":
+docker run -it --rm \
+    --network=mini_spark_broker_default \
+    -v $PWD/data:/home/jovyan/work/data:ro \
+    msb python bin/sendAlertStream.py kafka:9092 my-stream
