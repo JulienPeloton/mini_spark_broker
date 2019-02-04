821ceff90158debcc74833c65a506cf9b0c3a281
Julien <peloton@lal.in2p3.fr>
2019-02-04 15:56:06 +0100

    add Avro decoder for Spark, add routine to grab schema from avro file.

--- python/lsst/msb/avroUtils.py
+++ python/lsst/msb/avroUtils.py
@@ -1,44 +1,28 @@
+# Copyright 2018 Julien Peloton
+# Author: Maria Patterson, Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Utilities for manipulating Avro data and schemas.
+Some routines borrowed from lsst-dm/alert_stream.
 """
-
 import io
-import json
-import avro.schema
 import fastavro
 
-
-__all__ = ['combineSchemas', 'writeAvroData', 'readAvroData']
-
-
-def _loadSingleAvsc(file_path, names):
-    """Load a single avsc file.
-    """
-    with open(file_path) as file_text:
-        json_data = json.load(file_text)
-    schema = avro.schema.SchemaFromJSONData(json_data, names)
-    return schema
-
-
-def combineSchemas(schema_files):
-    """Combine multiple nested schemas into a single schema.
-
-    Parameters
-    ----------
-    schema_files : `list`
-        List of files containing schemas.
-        If nested, most internal schema must be first.
-
-    Returns
-    -------
-    `dict`
-        Avro schema
-    """
-    known_schemas = avro.schema.Names()
-
-    for s in schema_files:
-        schema = _loadSingleAvsc(s, known_schemas)
-    return schema.to_json()
-
+__all__ = [
+    'writeAvroData',
+    'readAvroData',
+    'readSchemaData',
+    'readSchemaFromAvroFile']
 
 def writeAvroData(json_data, json_schema):
     """Encode json into Avro format given a schema.
@@ -59,7 +43,6 @@ def writeAvroData(json_data, json_schema):
     fastavro.schemaless_writer(bytes_io, json_schema, json_data)
     return bytes_io
 
-
 def readAvroData(bytes_io, json_schema):
     """Read data and decode with a given Avro schema.
 
@@ -79,7 +62,6 @@ def readAvroData(bytes_io, json_schema):
     message = fastavro.schemaless_reader(bytes_io, json_schema)
     return message
 
-
 def readSchemaData(bytes_io):
     """Read data that already has an Avro schema.
 
@@ -96,3 +78,40 @@ def readSchemaData(bytes_io):
     bytes_io.seek(0)
     message = fastavro.reader(bytes_io)
     return message
+
+def readSchemaFromAvroFile(fn):
+    """ Reach schema from a binary avro file.
+
+    Parameters
+    ----------
+    fn: str
+        Input Avro file with schema.
+
+    Returns
+    ----------
+    schema: dict
+        Dictionary (JSON) describing the schema.
+    """
+    with open(fn, mode='rb') as file_data:
+        data = readSchemaData(file_data)
+        schema = data.schema
+    return schema
+
+def decoder(msg, alert_schema):
+    """ Decode an alert from Kafka (avro format)
+
+    Parameters
+    ----------
+    msg: bytes-like object (`_io.BytesIO`)
+        Message coming from Kafka.
+    alert_schema: dict
+        Dictionary (JSON) containing the schema of the message.
+
+    Returns
+    ----------
+    alert: dict
+        Dictionary describing the alert.
+    """
+    bytes_io = io.BytesIO(msg)
+    alert = readAvroData(bytes_io, alert_schema)
+    return alert
