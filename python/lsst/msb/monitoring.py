161c6b7f59ed86c18e90a0bc3defcf71a60f43f2
Julien <peloton@lal.in2p3.fr>
2019-02-12 08:54:11 +0100

    Add monitoring helper routine

---
+++ python/lsst/msb/monitoring.py
@@ -0,0 +1,70 @@
+# Copyright 2018 Julien Peloton
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pyspark.sql.streaming import StreamingQuery
+
+import pandas as pd
+
+import time
+
+def recentrecentProgress(query: StreamingQuery, colnames: list, waitfor: int=0):
+    """ Register recent query progresses in a Pandas DataFrame.
+
+    It turns out that Structured Streaming cannot be monitored as Streaming
+    in the Spark UI (which is a shame...), hence this simple routine to be
+    able to quickly plot progress.
+
+    Parameters
+    ----------
+    query: StreamingQuery
+        StreamingQuery query.
+    colnames: list of str
+        Fields of the query.recentProgress to be registered
+    waitfor: int, optional
+        If specified, wait for `waitfor` seconds. Default is 0.
+
+    Returns
+    ----------
+    data: pd.DataFrame
+        Pandas DataFrame whose columns are colnames, and index
+        is the timestamp.
+    """
+    time.sleep(waitfor)
+
+    # Force to register timestamp
+    if "timestamp" not in colnames:
+        colnames.append(timestamp)
+
+    # Register fields in a dic
+    dicval = {i: [] for i in colnames}
+    timestamp = []
+    for c in query.recentProgress:
+        try:
+            for colname in colnames:
+                dicval[colname].append(c[colname])
+        except (TypeError, KeyError):
+            # This can happen if the stream has not begun
+            # or is stuck.
+            continue
+
+    # Build DataFrame from dic
+    data = pd.DataFrame(dicval)
+
+    # Set timestamp as index
+    data.set_index('timestamp',inplace=True)
+
+    # Format it as datetime (useful for plot)
+    data.index = pd.to_datetime(data.index)
+
+    return data
