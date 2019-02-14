0854a30a24967f032214fd39f7ba93c04218eb5e
Julien <peloton@lal.in2p3.fr>
2019-02-14 14:40:59 +0100

    non-docker version versionfor monitoring the stream

--- launch_monitoring.sh
+++ launch_monitoring.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 ###################################
-# LSST Alert System
+# Monitor the stream (non-docker version)
 ###################################
-
-docker build -t "msb" .
 
 # Kafka dependencies
 KFKSTREAM=org.apache.spark:spark-streaming-kafka-0-10-assembly_2.10:2.2.0
 KFKSQL=org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0
 
 # Kafka producer stream location
-IPPORT="kafka:9092"
-# IPPORT="134.158.74.95:24499"
+IPPORT="134.158.74.95:24499"
+MASTER="local[*]"
 
 # Send bursts of alerts at expected visit intervals to topic "my-stream":
-docker run -it --rm \
-    --network=mini_spark_broker_default \
-    -v $PWD/data:/home/jovyan/work/data:ro \
-    msb /usr/local/spark/bin/spark-submit --master local[*] \
-    --packages ${KFKSTREAM},${KFKSQL} bin/monitorStream.py ${IPPORT} ztf-stream 5
+spark-submit --master ${MASTER} \
+    --packages ${KFKSTREAM},${KFKSQL} \
+    bin/monitorStream.py ${IPPORT} ztf-stream 5
