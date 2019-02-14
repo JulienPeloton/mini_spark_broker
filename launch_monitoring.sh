7953eb96981ddf410c63968356e96e7d1131fd49
Julien <peloton@lal.in2p3.fr>
2019-02-14 14:38:59 +0100

    Add ip:port for the kafka cluster

--- launch_monitoring.sh
+++ launch_monitoring.sh
@@ -9,9 +9,13 @@ docker build -t "msb" .
 KFKSTREAM=org.apache.spark:spark-streaming-kafka-0-10-assembly_2.10:2.2.0
 KFKSQL=org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0
 
+# Kafka producer stream location
+IPPORT="kafka:9092"
+# IPPORT="134.158.74.95:24499"
+
 # Send bursts of alerts at expected visit intervals to topic "my-stream":
 docker run -it --rm \
     --network=mini_spark_broker_default \
     -v $PWD/data:/home/jovyan/work/data:ro \
     msb /usr/local/spark/bin/spark-submit --master local[*] \
-    --packages ${KFKSTREAM},${KFKSQL} bin/monitorStream.py kafka:9092 ztf-stream 5
+    --packages ${KFKSTREAM},${KFKSQL} bin/monitorStream.py ${IPPORT} ztf-stream 5
